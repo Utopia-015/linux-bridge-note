e99bf6a19bc2e1a671399551e25c775752ea77a3
Utopia-015 <42064997+Utopia-015@users.noreply.github.com>
2023-12-09 16:43:04 +0800

    Update multidial.sh

--- multidial.sh
+++ multidial.sh
@@ -7,25 +7,22 @@ startnum=$5
 stopnum=$6
 MACADDR=40:00:02:$(openssl rand -hex 3 | sed 's/\(..\)/\1:/g; s/.$//')
 
-ip link show ${DEV}.${VLANID}
-if [ $? -eq 0 ]; then
-    ip link set ${DEV}.${VLANID} up
-else
+ip link show ${DEV}.${VLANID} > /dev/null &&
+    ip link set ${DEV}.${VLANID} up || {
     ip link set ${DEV} up
     ip link add link ${DEV} name ${DEV}.${VLANID} type vlan id ${VLANID}
     ip link set dev ${DEV}.${VLANID} address ${MACADDR}
     ip link set ${DEV}.${VLANID} up
-fi
+    }
 
-for i in `seq $startnum $stopnum`;do
-ip link show macvlan$i
-if [ $? -eq 0 ]; then
-    ip link set macvlan$i up
-else
-    ip link add link ${DEV}.${VLANID} macvlan$i type macvlan
-    ip link set macvlan$i up
-fi
-cat > /etc/ppp/peers/ppp${i} <<EOF
+for i in $(seq $startnum $stopnum); do
+    ip link show macvlan$i > /dev/null &&
+        ip link set macvlan$i up || {
+        ip link add link ${DEV}.${VLANID} macvlan$i type macvlan
+        ip link set macvlan$i up
+        }
+
+    cat >/etc/ppp/peers/ppp${i} <<EOF
 unit $i
 +ipv6
 nodefaultroute
@@ -38,4 +35,5 @@ mru 1492
 plugin rp-pppoe.so
 nic-macvlan$i
 EOF
+
 done
